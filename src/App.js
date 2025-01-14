5ad2bfe3b5106366fe4dfed80a64c76fa45c85dc
ranniepavillon <ranniepavillon@gmail.com>
2025-01-14 17:50:51 +0800

    test2345

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import { Helmet } from "react-helmet";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import './App.css';
 
 function App() {
-    const imageURL = "https://pofsis-business.s3.amazonaws.com/biz/1675245859953.png";
+    // const imageURL = "https://pofsis-business.s3.amazonaws.com/biz/1675245859953.png";
     // const dynamicImage = "https://pofsis-business.s3.amazonaws.com/biz/1675245819673.png";
     // <meta name="og:image" content="https://pofsis-business.s3.amazonaws.com/biz/1675245819673.png" />
     return (
-        <div>
+        <HelmetProvider>
             <Helmet>
-                <meta property="og:image" content={imageURL} />
-                <meta property="og:image:alt" content="Description of the image" />
-                <meta property="og:title" content="Your Page Title" />
-                <meta property="og:description" content="Your Page Description" />
+                <meta property="og:title" content="https://pofsis-business.s3.amazonaws.com/biz/1675245859953.png" />
+                <meta property="og:description" content="Dynamic Page Description" />
+                <meta property="og:image" content="https://example.com/image.jpg" />
             </Helmet>
-            <h1>Rannie Testing revised application234</h1>
+            <h1>Rannie Testing revised application2345</h1>
             <p>Check the Open Graph meta tags using a debugger tool.</p>
-        </div>
+        </HelmetProvider>
+        // <div>
+        //     <Helmet>
+        //         <meta property="og:image" content={imageURL} />
+        //         <meta property="og:image:alt" content="Description of the image" />
+        //         <meta property="og:title" content="Your Page Title" />
+        //         <meta property="og:description" content="Your Page Description" />
+        //     </Helmet>
+        //     <h1>Rannie Testing revised application234</h1>
+        //     <p>Check the Open Graph meta tags using a debugger tool.</p>
+        // </div>
     );
 }
 
