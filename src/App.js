fd2b5dc02b569dc5474655ccc0fb254613f6c051
ranniepavillon <ranniepavillon@gmail.com>
2025-01-14 17:43:08 +0800

    test234

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import { Helmet } from "react-helmet";
 import './App.css';
 
 function App() {
-    const dynamicImage = "https://pofsis-business.s3.amazonaws.com/biz/1675245859953.png";
+    const imageURL = "https://pofsis-business.s3.amazonaws.com/biz/1675245859953.png";
     // const dynamicImage = "https://pofsis-business.s3.amazonaws.com/biz/1675245819673.png";
     // <meta name="og:image" content="https://pofsis-business.s3.amazonaws.com/biz/1675245819673.png" />
     return (
         <div>
             <Helmet>
-                <meta name="og:image" content={dynamicImage} />
-                <meta property="og:title" content="Dynamic OG Image Example" />
-                <meta property="og:description" content="This is a dynamically set Open Graph image." />
+                <meta property="og:image" content={imageURL} />
+                <meta property="og:image:alt" content="Description of the image" />
+                <meta property="og:title" content="Your Page Title" />
+                <meta property="og:description" content="Your Page Description" />
             </Helmet>
-            <h1>Rannie Testing revised application23</h1>
+            <h1>Rannie Testing revised application234</h1>
             <p>Check the Open Graph meta tags using a debugger tool.</p>
         </div>
     );
